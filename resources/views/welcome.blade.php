e1fe3ae7f43ef8f2317afb7365a80b226a54dc8c
tim <timlubchak@gmail.com>
2017-09-03 22:48:02 +0300

    Commit3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel My first site
                 </div>
 
                 <div class="links">
